b27481c516ebdd6a23ee7fe44ce8fb46d4f681bb
Amir Momenian <nainemom@gmail.com>
2020-03-29 01:02:03 +0430

    create input component

---
+++ src/components/Input.vue
@@ -0,0 +1,31 @@
+<template>
+<input :class="$style.input" :value="value" @input="$emit('input', $event.target.value)" />
+</template>
+
+<script>
+
+export default {
+  props: {
+    value: {
+      type: String,
+    },
+  },
+  style({ className, mediaQuery }) {
+    return [
+      className('input', {
+        borderRadius: '8px',
+        border: `solid 1px ${this.$root.theme.borderColor}`,
+        padding: '8px',
+        height: '48px',
+        fontSize: '1em',
+        display: 'block',
+        width: '100%',
+        '&[disabled]': {
+          background: '#fff',
+          cursor: 'not-allowed',
+        },
+      }),
+    ];
+  }
+}
+</script>
