2f992d3eb607c4574cffe9403d251bd2f5597bc6
Amir Momenian <nainemom@gmail.com>
2020-03-29 01:03:00 +0430

    use input component

--- src/components/MessageForm.vue
+++ src/components/MessageForm.vue
@@ -1,13 +1,17 @@
 <template>
 <form @submit.prevent="submit" :class="$style.container">
-  <input :class="$style.messageInput" placeholder="Enter Your Message..." :value="value" @input="$emit('input', $event.target.value)" required :disabled="disabled"/>
+  <Input :class="$style.messageInput" placeholder="Enter Your Message..." :value="value" @input="$emit('input', $event.target.value)" required :disabled="disabled"/>
   <button :class="$style.submitButton" type="submit" :disabled="disabled"> <i class="fa fa-paper-plane" /> </button>
 </form>
 </template>
 
 <script>
+import Input from './Input.vue';
 
 export default {
+  components: {
+    Input,
+  },
   props: {
     value: {
       type: String,
@@ -31,13 +35,6 @@ export default {
         flexGrow: 1,
         marginRight: '8px',
         borderRadius: '8px',
-        border: `solid 1px ${this.$root.theme.borderColor}`,
-        padding: '8px',
-        height: '48px',
-        '&[disabled]': {
-          background: '#fff',
-          cursor: 'not-allowed',
-        },
       }),
       className('submitButton', {
         borderRadius: '24px',
