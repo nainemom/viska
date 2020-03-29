e51d2edce3637d5c10d070aace6b2fe701786f15
Amir Momenian <nainemom@gmail.com>
2020-03-29 14:25:33 +0430

    create button component

---
+++ src/components/Button.vue
@@ -0,0 +1,66 @@
+<template>
+<button :class="[$style.button, color, fullWidth && 'full-width', fab && 'fab']">
+  <div v-if="loading">
+    <i class="fa fa-spinner fa-pulse" />
+  </div>
+  <slot v-else/>
+</button>
+</template>
+
+<script>
+
+export default {
+  props: {
+    color: {
+      type: String,
+      default: 'default',
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    fab: {
+      type: Boolean,
+      default: false
+    },
+    fullWidth: {
+      type: Boolean,
+      default: false
+    },
+  },
+  style({ className, mediaQuery }) {
+    return [
+      className('button', {
+        display: 'inline-block',
+        borderRadius: '8px',
+        // minHeight: '48px',
+        border: `solid 1px ${this.$root.theme.borderColor}`,
+        textAlign: 'center',
+        cursor: 'pointer',
+        padding: '0 16px',
+        lineHeight: 1.6,
+        fontSize: '16px',
+        width: 'auto',
+        '&.full-width': {
+          width: '100%',
+        },
+        '&.primary': {
+          background: this.$root.theme.primaryColor,
+          color: '#fff',
+        },
+        '&.default': {
+          background: 'transparent',
+          color: 'inherit',
+        },
+        '&.default': {
+          background: this.$root.theme.backgroundColor,
+          color: 'inherit',
+        },
+        '&[disabled]': {
+          cursor: 'not-allowed',
+        },
+      }),
+    ];
+  }
+}
+</script>
