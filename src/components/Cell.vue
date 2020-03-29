60e85d7c03d5be01baf4b9dfcd2817048ada807c
Amir Momenian <nainemom@gmail.com>
2020-03-29 14:25:45 +0430

    remove paddings

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -12,10 +12,10 @@ export default {
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'center',
-        padding: '0 8px',
+        // padding: '0 8px',
         overflow: 'hidden',
         '& > .item': {
-          margin: '0 8px',
+          // margin: '0 8px',
         },
         '& > .grow': {
           flexGrow: 1,
