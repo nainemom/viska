c248c2d02c343ff22fe3906d241513b841a8ae73
Amir Momenian <nainemom@gmail.com>
2020-04-07 13:53:25 +0430

    refactor using js Map

--- server/store.js
+++ server/store.js
@@ -2,51 +2,28 @@
 
 const store = {};
 
+const generateStrKey = (obj) => Object.keys(obj).sort().map((key) => `${key}:${obj[key]}`).join(',');
+
 const create = (name) => {
-  store[name] = [];
+  store[name] = new Map();
   return {
     _store: store[name],
-    insert(document) {
-      store[name].push(document);
+    set(key, data) {
+      store[name].set(generateStrKey(key), data);
     },
-    find(searchObj) {
-      const searchKeys = Object.keys(searchObj);
-      for(let i = 0; i < store[name].length; i++) {
-        const document = store[name][i];
-        let match = true;
-        for(let j = 0; j < searchKeys.length; j++) {
-          const searchKey = searchKeys[j];
-          if (document[searchKey] !== searchObj[searchKey]) {
-            match = false;
-            break;
-          }
-        }
-        if (match) {
-          return document;
-        }
-      }
-      return undefined;
+    put(key, data) {
+      const strKey = generateStrKey(key);
+      store[name].set(strKey, [
+        ...(store[name].get(strKey) || []),
+        data
+      ]);
     },
-    delete(searchObj) {
-      const searchKeys = Object.keys(searchObj);
-      let length = store[name].length
-      for(let i = 0; i < length; i++) {
-        const document = store[name][i];
-        let match = true;
-        for(let j = 0; j < searchKeys.length; j++) {
-          const searchKey = searchKeys[j];
-          if (document[searchKey] !== searchObj[searchKey]) {
-            match = false;
-            break;
-          }
-        }
-        if (match) {
-          store[name].splice(i, 1);
-          i--;
-          length--;
-        }
-      }
-    }
+    get(key) {
+      return store[name].get(generateStrKey(key));
+    },
+    delete(key) {
+      return store[name].delete(generateStrKey(key));
+    },
   }
 }
 
