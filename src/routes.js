1796ec242da848c77ec55b44049354bd2e012791
Amir Momenian <nainemom@gmail.com>
2020-04-13 16:30:53 +0430

    change routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import Home from './pages/Home.vue';
 export default [
   { path: '/', component: Home },
-  { path: '/chats', component: Home },
-  { path: '/chats/:user', component: Home },
+  { path: '/:user', component: Home },
   { path: '/*', component: Home },
 ];
