f5ec4e2504729662aaa3f58275ef27045ffdef69
Arash Hatami <hatamiarash7@gmail.com>
2020-04-15 02:59:52 +0430

    remove boxShadow

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,70 +1,48 @@
 <template>
-  <div :class="$style.container">
-    <Cell :class="$style.header" v-if="chat">
-      <Cell class="padding-x-sm size-lg grow">
-        <div class="padding-right-md">
-          <UserAvatar :user="chat.user" :size="48" />
-        </div>
-        <div class="padding-right-md">
-          <div>
-            <UserName :user="chat.user" />
-          </div>
-        </div>
-        <div class="padding-x-sm"><StatusIcon :value="chat.isOnline" /></div>
-      </Cell>
-      <Button
-        class="size-lg padding-lg"
-        color="text-danger"
-        @click.native="removeChat"
-      >
-        <i class="fa fa-trash" />
-        <span :class="$style.hiddenOnMobile"> Delete </span>
-      </Button>
-      <Button
-        class="size-lg padding-lg"
-        color="transparent"
-        @click.native="closeChat"
-      >
-        <i class="fa fa-times" />
-        <span :class="$style.hiddenOnMobile"> Close </span>
-      </Button>
-    </Cell>
-    <div :class="$style.conversation" class="padding-top-lg" ref="conversation">
-      <div
-        v-for="(message, index) in messages"
-        :key="'m' + index"
-        :class="[$style.messageItem, message.from, calcTextDir(message.body)]"
-      >
-        <div class="inside padding-top-md padding-x-md margin-y-xs text-md">
-          {{ message.body }}
-          <div class="info padding-bottom-md padding-top-sm">
-            {{ message.date | datetime }}
-          </div>
+<div :class="$style.container">
+  <Cell :class="$style.header" v-if="chat">
+    <Cell class="padding-x-sm size-lg grow">
+      <div class="padding-right-md">
+        <UserAvatar :user="chat.user" :size="48" />
+      </div>
+      <div class="padding-right-md">
+        <div>
+          <UserName :user="chat.user" />
         </div>
       </div>
-      <div :class="$style.isTyping" class="padding-sm">
-        <span v-if="itIsTyping"> {{ chat.user.name }} is typing... </span>
+      <div class="padding-x-sm"><StatusIcon :value="chat.isOnline" /></div>
+    </Cell>
+    <Button class="size-lg padding-lg" color="text-danger" @click.native="removeChat">
+      <i class="fa fa-trash" /> <span :class="$style.hiddenOnMobile"> Delete </span>
+    </Button>
+    <Button class="size-lg padding-lg" color="transparent" @click.native="closeChat">
+      <i class="fa fa-times" /> <span :class="$style.hiddenOnMobile"> Close </span>
+    </Button>
+  </Cell>
+  <div :class="$style.conversation" class="padding-top-lg" ref="conversation">
+    <div v-for="(message, index) in messages" :key="'m' + index" :class="[$style.messageItem, message.from, calcTextDir(message.body)]">
+      <div class="inside padding-top-md padding-x-md margin-y-xs text-md">
+        {{ message.body }}
+        <div class="info padding-bottom-md padding-top-sm"> {{ message.date | datetime }} </div>
       </div>
     </div>
-    <MessageForm
-      :class="[$style.messageForm, calcTextDir(inputText)]"
-      class="padding-lg"
-      @submit="sendMessage"
-      :value="inputText"
-      @input="onInput"
-      :disabled="!chat"
-    />
+    <div :class="$style.isTyping" class="padding-sm">
+      <span  v-if="itIsTyping">
+        {{ chat.user.name }} is typing...
+      </span>
+    </div>
   </div>
+  <MessageForm :class="[$style.messageForm, calcTextDir(inputText)]" class="padding-lg" @submit="sendMessage" :value="inputText" @input="onInput" :disabled="!chat" />
+</div>
 </template>
 
 <script>
-import Cell from "./Cell.vue";
-import Button from "./Button.vue";
-import UserAvatar from "./UserAvatar.vue";
-import UserName from "./UserName.vue";
-import StatusIcon from "./StatusIcon.vue";
-import MessageForm from "./MessageForm.vue";
-import calcTextDirection from "../../utils/calcTextDirection.js";
+import Cell from './Cell.vue';
+import Button from './Button.vue';
+import UserAvatar from './UserAvatar.vue';
+import UserName from './UserName.vue';import StatusIcon from './StatusIcon.vue';
+import MessageForm from './MessageForm.vue';
+import calcTextDirection from '../../utils/calcTextDirection.js';
 
 export default {
   components: {
@@ -73,20 +51,20 @@ export default {
     UserAvatar,
     UserName,
     StatusIcon,
-    MessageForm
+    MessageForm,
   },
   props: {
     chat: {
-      type: Object
-    }
+      type: Object,
+    },
   },
   data() {
     return {
-      inputText: "",
+      inputText: '',
       lastIsTypingFlagSent: 0,
       itIsTyping: false,
-      isTypingTimer: undefined
-    };
+      isTypingTimer: undefined,
+    }
   },
   computed: {
     messages() {
@@ -108,7 +86,7 @@ export default {
     sendMessage() {
       if (this.chat) {
         const message = this.inputText;
-        this.inputText = "";
+        this.inputText = '';
         this.$chatService.sendMessage(this.chat, message).then();
       }
     },
@@ -120,22 +98,18 @@ export default {
       }
     },
     closeChat() {
-      this.$emit("close");
+      this.$emit('close');
     },
     removeChat() {
-      this.$emit("remove");
+      this.$emit('remove');
     },
     scrollToEnd() {
       this.$refs.conversation.scrollTo(0, this.$refs.conversation.scrollHeight);
     }
   },
   mounted() {
-    this.$chatService.$on("isTypingFlag", user => {
-      if (
-        this.chat &&
-        this.chat.user.username === user.username &&
-        this.chat.user.type === user.type
-      ) {
+    this.$chatService.$on('isTypingFlag', (user) => {
+      if (this.chat && this.chat.user.username === user.username && this.chat.user.type === user.type) {
         this.itIsTyping = true;
         clearTimeout(this.isTypingTimer);
         this.isTypingTimer = setTimeout(() => {
@@ -151,17 +125,17 @@ export default {
         this.chat.badge = 0;
         this.$chatService._saveChats();
       }
-      this.inputText = "";
+      this.inputText = '';
       this.ItIsTyping = false;
       clearTimeout(this.isTypingTimer);
     },
-    "messages.length"() {
+    'messages.length'() {
       this.itIsTyping = false;
       this.$nextTick(() => {
         this.scrollToEnd();
       });
     },
-    "pendingMessages.length"() {
+    'pendingMessages.length'() {
       this.$nextTick(() => {
         this.scrollToEnd();
       });
@@ -176,116 +150,115 @@ export default {
   },
   style({ className, mediaQuery }) {
     return [
-      className("container", {
-        height: "100%",
-        display: "flex",
-        flexDirection: "column"
+      className('container', {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
       }),
-      className("header", {
+      className('header', {
         background: this.$root.theme.backgroundColor,
         borderBottom: `solid 1px ${this.$root.theme.borderColor}`,
         // color: '#fff',
         // minHeight: '81px', // flex problem
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        overflow: "hidden",
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        overflow: 'hidden',
         flexShrink: 0,
-        "& > .name": {
-          flexGrow: 1
+        '& > .name': {
+          flexGrow: 1,
         },
-        "& > .close-button": {
-          width: "48px",
-          height: "48px",
+        '& > .close-button': {
+          width: '48px',
+          height: '48px',
           // borderRadius: '8px',
-          border: "none",
-          backgroundColor: "transparent",
-          color: "inherit",
-          cursor: "pointer"
+          border: 'none',
+          backgroundColor: 'transparent',
+          color: 'inherit',
+          cursor: 'pointer',
         }
       }),
-      className("conversation", {
+      className('conversation', {
         flexGrow: 1,
-        overflowY: "scroll",
-        overflowX: "hidden",
+        overflowY: 'scroll',
+        overflowX: 'hidden',
         background: this.$root.theme.backgroundColor,
         // paddingBottom: '15px',
-        overflowWrap: "break-word",
-        lineHeight: 1.5
+        overflowWrap: 'break-word',
+        lineHeight: 1.5,
       }),
-      className("isTyping", {
-        position: "sticky",
+      className('isTyping', {
+        position: 'sticky',
         bottom: 0,
-        backgroundColor: this.$root.theme.backgroundColor
+        backgroundColor: this.$root.theme.backgroundColor,
       }),
-      className("messageItem", {
-        userSelect: "none",
+      className('messageItem', {
+        userSelect: 'none',
         // fontWeight: 'bold',
-        width: "100%",
-        "&.rtl": {
-          direction: "rtl"
+        width: '100%',
+        '&.rtl': {
+          direction: 'rtl',
         },
-        "& > .inside": {
+        '& > .inside': {
           // fontSize: '22px',
           // padding: '15px',
-          maxWidth: "70%",
-          display: "inline-block",
-          overflow: "hidden",
-          userSelect: "text",
-          minWidth: "100px",
-          "& > .info": {
+          maxWidth: '70%',
+          display: 'inline-block',
+          overflow: 'hidden',
+          userSelect: 'text',
+          minWidth: '100px',
+          '& > .info': {
             opacity: 0.5,
-            fontSize: "10px",
-            textAlign: "left",
-            display: "block"
+            fontSize: '10px',
+            textAlign: 'left',
+            display: 'block',
           },
-          "&:hover > .info": {
-            display: "block"
+          '&:hover > .info': {
+            display: 'block'
           }
         },
-        "&.its": {
-          textAlign: "left",
-          "& > .inside": {
+        '&.its': {
+          textAlign: 'left',
+          '& > .inside': {
             background: this.$root.theme.shadowColor,
-            color: "#111",
-            borderTopRightRadius: "8px",
-            borderTopLeftRadius: "8px",
-            borderBottomRightRadius: "8px",
+            color: '#111',
+            borderTopRightRadius: '8px',
+            borderBottomRightRadius: '8px',
+            borderBottomLeftRadius: '8px',
             marginLeft: "8px",
-            boxShadow: "0px 2px 1px rgba(0, 0, 0, 0.2)",
-            "& > .info": {
-              textAlign: "right"
+            '& > .info': {
+              textAlign: 'right',
             }
           }
         },
-        "&.me": {
-          textAlign: "right",
-          "& > .inside": {
+        '&.me': {
+          textAlign: 'right',
+          '& > .inside': {
             background: this.$root.theme.primaryColor,
-            color: "#fff",
-            borderTopLeftRadius: "8px",
-            borderBottomLeftRadius: "8px",
+            color: '#fff',
+            borderTopLeftRadius: '8px',
+            borderBottomLeftRadius: '8px',
             borderTopRightRadius: "8px",
             marginRight: "8px",
-            boxShadow: "0px 2px 1px rgba(0, 0, 0, 0.2)"
           }
-        }
+        },
+
       }),
-      className("messageForm", {
+      className('messageForm', {
         // padding: '15px',
         background: this.$root.theme.backgroundColor2,
         borderTop: `solid 1px ${this.$root.theme.borderColor}`,
         flexShrink: 0,
-        "&.rtl input": {
-          direction: "rtl"
+        '&.rtl input': {
+          direction: 'rtl',
         }
       }),
-      mediaQuery({ maxWidth: "600px" }, [
-        className("hiddenOnMobile", {
-          display: "none"
-        })
-      ])
+      mediaQuery({ maxWidth: '600px' }, [
+        className('hiddenOnMobile', {
+          display: 'none',
+        }),
+      ]),
     ];
-  }
-};
+  },
+}
 </script>
