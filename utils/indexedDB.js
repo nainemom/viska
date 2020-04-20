c54b7cfe5f41d4be6e011e4f67128f11644dc68d
Amir Momenian <nainemom@gmail.com>
2020-04-20 19:46:49 +0430

    import old loki db (temporary)

--- utils/indexedDB.js
+++ utils/indexedDB.js
@@ -1,11 +1,30 @@
-import { openDB } from 'idb';
+import { openDB, deleteDB } from 'idb';
 
-
+// migrating from loki. should be remove later.
+// TODO remove this shit
+const loadLokiData = (uname, deleteAfter) => new Promise((_resolve) => {
+  openDB('LokiCatalog', 1).then((oldDb) => {
+    oldDb.get('LokiAKV', 1).then((data) => {
+      const dt = data.val ? JSON.parse(data.val) : {};
+      // really bad, stupid and dangerious way.
+      if (dt.filename.includes(uname) && dt.collections && dt.collections.length) {
+        const chats = dt.collections[0].data.map((_chat) => ({
+          badge: _chat.badge,
+          isOnline: null,
+          messages: _chat.messages,
+          user: _chat.user,
+        }));
+        _resolve(chats);
+        deleteAfter && deleteDB('LokiCatalog');
+      }
+    }).catch(() => _resolve([]));
+  }).catch(() => _resolve([]));
+});
 
 export default ({
   name = 'db',
   version = 1,
-  prefix = 'prefix',
+  prefix = '',
   dynamicCollections = [],
   staticCollections = [],
 }) => {
@@ -17,11 +36,9 @@ export default ({
 
   const dbPromise = new Promise((resolve) => {
     openDB(name, version, {
-      upgrade(db) {
+      async upgrade(db) {
         collections.forEach((collection) => {
-          if (db.objectStoreNames.contains(collection)) {
-            // react on version change
-          } else {
+          if (!db.objectStoreNames.contains(collection)) {
             db.createObjectStore(collection);
           }
         });
@@ -49,8 +66,26 @@ export default ({
   });
 
   const initCollection = (collection) => {
-    const get = () => dbPromise.then((db) => db.get(collection, isDynamic(collection) ? prefix : 'root'));
     const set = (val) => dbPromise.then((db) => db.put(collection, val, isDynamic(collection) ? prefix : 'root'));
+    const get = () => {
+      // TODO also remove this shit (if{} block)
+      if (collection === 'chats' && prefix) {
+        return new Promise(_resolve => {
+          loadLokiData(prefix.substr(1), true).then((oldChats) => {
+            dbPromise.then((db) => db.get(collection, isDynamic(collection) ? prefix : 'root')).then((newChats) => {
+              const parsedDt = [
+                ...newChats,
+                ...oldChats,
+              ].filter((_chat, i, h) => h.findIndex((__chat) => __chat.user.username === _chat.user.username) === i)
+              set(parsedDt).then(() => {
+                _resolve(parsedDt);
+              });
+            })
+          });
+        });
+      }
+      dbPromise.then((db) => db.get(collection, isDynamic(collection) ? prefix : 'root'));
+    }
     const clear = () => dbPromise.then((db) => db.clear(collection, isDynamic(collection) ? prefix : 'root'));
 
     return {
@@ -59,7 +94,6 @@ export default ({
       clear,
     }
   }
-
 
   const ret = {};
   collections.forEach((collection) => {
