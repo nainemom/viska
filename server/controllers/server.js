1a93aa4b95a2ac835ca571971e81f0bdacf62f02
Amir Momenian <nainemom@gmail.com>
2022-10-07 18:57:35 +0330

    add public key to server status

--- server/controllers/server.js
+++ server/controllers/server.js
@@ -1,8 +1,11 @@
 import { version } from '../../package.json';
+import { SERVER_ENTER_KEY } from '../constants/index.js';
+
 
 export const getStatus = async ({ res, users }) => {
   res.json({
     connections: users.size,
     version,
+    public: SERVER_ENTER_KEY.length > 0,
   });
 };
