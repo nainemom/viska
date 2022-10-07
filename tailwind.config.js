e05268c839efd6f6bd35a115264a84b4405a9136
Amir Momenian <nainemom@gmail.com>
2022-10-07 15:45:38 +0330

    start refactoring

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./client/**/*.{js,jsx,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
